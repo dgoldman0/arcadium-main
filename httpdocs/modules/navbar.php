ac8751c3ee6403588b767bd34e57de6d444eec9c
dgoldman0 <admin@danielgoldman.us>
2020-09-15 13:42:54 -0400

    Rearrangements

---
+++ httpdocs/modules/navbar.php
@@ -0,0 +1,29 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="index.php"><img src="assets/img/logo.png" width="30px" height="30px" /> wrldcoin.io</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor02">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-toolbox text-white"></i> External</a>
+                    <div class="dropdown-menu bg-primary" aria-labelledby="dropdown04">
+                        <a class="dropdown-item text-white" href="http://worldbuilder.fun/"> Mini Games</a>
+                        <a class="dropdown-item text-white" href="https://nolex.org/main/WRLDTRX.html?ref=5f27039c6766fa6f943137be"> WRLD on NoleX</a>
+                    </div>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="forge.php"><i class="fas fa-fire-alt text-white"></i> Forge</a></li>
+                <li class="nav-item"><a class="nav-link" href="wrapped.php"><i class="fas fa-arrows-alt-h text-white"></i> WWRLD</a></li>
+            </ul>
+
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a class="nav-link" href="https://twitter.com/wrldcoin" target="_blank"><i class="fab fa-twitter text-white"></i> Twitter</a></li>
+                <li class="nav-item"><a class="nav-link" href="/wrld-whitepaper.pdf" target="_blank"><i class="fas fa-file-contract text-white"></i> Whitepaper</a></li>
+                <!--<li class="nav-item"><a class="nav-link" href="https://github.com/#" target="_blank"><i class="fab fa-github text-white"></i> GitHub</a></li>
+                <li class="nav-item"><a class="nav-link" href="#" target="_blank"><i class="fas fa-search-dollar text-white"></i> Contract</a></li>-->
+            </ul>
+        </div>
+    </nav>
+</header>
