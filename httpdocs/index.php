d7e6940a546b15d3407ab967fa8b612e4dc1ae04
dgoldman0 <admin@danielgoldman.us>
2020-11-17 11:39:17 -0500

    Update index.php

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -6,7 +6,7 @@ It might also be interesting to allow people to create their own contract withou
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Arcadium Engine</title>
+        <title>Arcadium Project</title>
 
         <!-- CHARSET AND VIEWPORT METAS AT THE TOP, BELOW TITLE ALWAYS. -->
         <meta charset="utf8">
@@ -37,19 +37,24 @@ It might also be interesting to allow people to create their own contract withou
                 <div class="container">
                     <div class="row">
                         <div class="col text-left">
-                            <h1 class="text-white">Arcadium Engine</h1>
-                            <p class="text-success">A fun gaming and entertainment experience for blockchain professionals and novices alike.</p>
+                            <h1 class="text-white">Arcadium Project</h1>
+                            <p class="text-success">A World Builder Project Supporting Entertainment</p>
                         </div>
                     </div>
                 </div>
             </div>
 
             <div class="row">
-                <div class="col-md-12">
+              <div class="col-md-12">
+                <div class="card glassPanel border-light rcAll">
+                  <div class="card-header glassPanel text-white rcTop">About Arcadium</div>
+                  <div class="card-body text-white">
+                    The entertainment industry is massive. And it should be. Entertainment is not a lurxury, but a necessity. It helps us relax, and reduce negative stress. It is good for our health. Arcadium's mission is to use blockchain technology to improve the entertainment industry. We plan on offering many services to game developers, streamers, and other kinds of entertainers. We seek to make entertainment easier to access, more enjoyable, and better for everyone.
+                  </div>
                 </div>
+              </div>
             </div>
         </div>
-
         <!-- Project Dependencies -->
         <script src="assets/js/jquery.min.js"></script>
         <script src="assets/js/popper.min.js"></script>
