5e0257e90ee8f19b58d0b3bc3864695cb3c00ef2
dgoldman0 <admin@danielgoldman.us>
2020-11-27 14:40:23 -0500

    Battle summary progress
    Working on adding the ability to look thrugh past battles.

---
+++ httpdocs/bot/assets/js/db.js
@@ -0,0 +1,11 @@
+async function init() {
+}
+
+// Get a list of past battles for a given channel
+async function getBattles(channel) {
+
+}
+
+async function getBattle(battle_id) {
+
+}
---
+++ httpdocs/bot/battle.php
@@ -0,0 +1,92 @@
+<!--
+I want to allow someone to enter a custom token address. That one will be easy to implement through a selector option "custom" which temporarily adds. I also want to create a whitelist system.
+
+It might also be interesting to allow people to create their own contract without coding, using a basic form to enter some details, kind of like JustSwap. I'd have to learn how to custom compile code though.
+-->
+<?php
+$m = new MongoClient(); // connect
+$db = $m->selectDB("bot-battles");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Arcadium Engine</title>
+
+        <!-- CHARSET AND VIEWPORT METAS AT THE TOP, BELOW TITLE ALWAYS. -->
+        <meta charset="utf8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+
+        <!-- NOW SOME DESCRIPTION METAS -->
+        <meta name="description" content="Arcadium Blockchain Game Engine">
+        <meta name="keywords" content="cryptocurrency, crypto, blockchain, games, arcade">
+
+        <!-- CSS, ICONS AND BOOTSTRAP -->
+        <link rel="stylesheet" href="https://bootswatch.com/4/pulse/bootstrap.min.css" />
+        <link rel="stylesheet" href="assets/css/arcadium.css" />
+        <link rel="icon" type="image/png" href="assets/img/etc.png">
+
+        <!-- FontAwesome -->
+        <script src="https://kit.fontawesome.com/f352960784.js" crossorigin="anonymous"></script>
+    </head>
+
+    <body class="wallpaper">
+        <?php include('modules/navbar.php'); ?>
+
+        <!-- SPACING OF 5 BR'S NECESSARY FOR TOP OF CONTENT TO SHOW WITH STICKY NAVBAR. -->
+        <br /><br /><br /><br /><br />
+
+        <!-- MAIN CONTENT DIV -->
+        <div class="container">
+            <div class="row jumbotron glassPanel rcAll">
+                <div class="container">
+                    <div class="row">
+                        <div class="col text-left">
+                            <h1 class="text-white">Arcadium Bot</h1>
+                            <p class="text-success">Arcadium Bot is a Twitch bot with a lot of fun commands.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12">
+                  <div class="card glassPanel border-light rcAll">
+                    <div class="card-header glassPanel text-white rcTop">Battle History</div>
+                    <div class="card-body text-white">
+                      <table>
+                        <tr>
+                          <th>Battle ID</th>
+                          <th>Winner</th>
+                        </tr>
+                        <?php
+                          $channel = $_GET["channel"];
+                          if (isset(channel)) {
+                            $query = array('channel' => channel);
+                            $cursor = $db->battles->find($query);
+                            foreach ($cursor as $doc) {
+                              ?>
+                              <tr>
+                                <td>$cursor->_id</td>
+                                <td>$cursor->winner</td>
+                              </tr>
+                              <?php
+                            }
+                          }
+                        ?>
+                      </table>
+                    </div>
+                  </div>
+              </div>
+            </div>
+        </div>
+        <!-- Project Dependencies -->
+        <script src="/assets/js/jquery.min.js"></script>
+        <script src="/assets/js/popper.min.js"></script>
+        <script src="/assets/js/bootstrap.min.js"></script>
+        <script src="/assets/js/tronweb.js"></script>
+
+        <!-- Dapp Scripts -->
+        <script src = 'assets/js/db.js'></script>
+    </body>
+</html>
