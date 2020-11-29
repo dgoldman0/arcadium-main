e253a5c9ca2d6cf92df232c9acd2ebe001437d7a
dgoldman0 <admin@danielgoldman.us>
2020-11-29 11:21:49 -0500

    Fixed echo error

--- httpdocs/bot/battle.php
+++ httpdocs/bot/battle.php
@@ -15,7 +15,6 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
 ?>
 
 <!DOCTYPE html>
