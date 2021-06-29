9d3e56d6c5a5248678ed6ad8e2a75a1425714f78
dgoldman0 <45105852+dgoldman0@users.noreply.github.com>
2021-06-29 16:08:07 -0400

    Worked on Titles
    Added titles.sol (very incomplete) and titles.php for the site.

---
+++ httpdocs/titles.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Arcadium Twitch Bot</title>
+
+        <!-- CHARSET AND VIEWPORT METAS AT THE TOP, BELOW TITLE ALWAYS. -->
+        <meta charset="utf8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+
+        <!-- NOW SOME DESCRIPTION METAS -->
+        <meta name="description" content="Arcadium Titles">
+        <meta name="keywords" content="cryptocurrency, crypto, blockchain, games, arcade">
+
+        <!-- CSS, ICONS AND BOOTSTRAP -->
+        <link rel="stylesheet" href="https://bootswatch.com/4/pulse/bootstrap.min.css" />
+        <link rel="stylesheet" href="assets/css/arcadium.css" />
+        <link rel="icon" type="image/png" href="assets/img/etc.png">
+
+        <!-- FontAwesome -->
+        <script src="https://kit.fontawesome.com/f352960784.js" crossorigin="anonymous"></script>
+    </head>
+
+    <body class="wallpaper">
+        <?php include('modules/navbar.php'); ?>
+
+        <!-- SPACING OF 5 BR'S NECESSARY FOR TOP OF CONTENT TO SHOW WITH STICKY NAVBAR. -->
+        <br /><br /><br /><br /><br />
+
+        <!-- MAIN CONTENT DIV -->
+        <div class="container">
+            <div class="row jumbotron glassPanel rcAll">
+                <div class="container">
+                    <div class="row">
+                        <div class="col text-left">
+                            <h1 class="text-white">Introducing Arcadium Titles</h1>
+                            <p class="text-success">A Multi-Game NFT</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                  <div class="card glassPanel border-light rcAll">
+                    <div class="card-header glassPanel text-white rcTop">About</div>
+                    <div class="card-body text-white">
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Project Dependencies -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/popper.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/tronweb.js"></script>
+
+        <!-- Dapp Scripts -->
+    </body>
+</html>
