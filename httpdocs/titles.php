cffe9cdcdd15930bab5f0aec628ea5bbbd3967f4
dgoldman0 <45105852+dgoldman0@users.noreply.github.com>
2021-06-29 16:23:55 -0400

    Update titles.php

--- httpdocs/titles.php
+++ httpdocs/titles.php
@@ -43,11 +43,21 @@
                 <div class="card glassPanel border-light rcAll">
                 <div class="card-header glassPanel text-white rcTop">The Basics</div>
                 <div class="card-body text-white">
-                  Titles are an exciting addition coming to Arcadium in the near future. A title is a special NFT, with a few different functions. First, it shows status. Different ranks of titles will be available for purhcase or crafting. More importantly, these titles will unlock various features in many of our games. This feature makes titles fairly unique. Usually NFTs are meant to be used in a single product. Titles will be useful in a various of dapps created by Arcadium and its partners.    
+                  Titles are an exciting addition coming to Arcadium in the near future. A title is a special NFT, with a few different functions. First, it shows status. Different ranks of titles will be available for purhcase or crafting. More importantly, these titles will unlock various features in many of our games. This feature makes titles fairly unique. Usually NFTs are meant to be used in a single product. Titles will be useful in a various of dapps created by Arcadium and its partners.
                 </div>
               </div>
             </div>
           </div>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card glassPanel border-light rcAll">
+              <div class="card-header glassPanel text-white rcTop">Staking</div>
+              <div class="card-body text-white">
+                Titles will also have an important staking element. In order to create a title of a given rank, a user needs two things: a title of rank one higher than the one being created, and a certain amount of ARC tokens. Part of that ARC will be locked away forever. The rest will be distributed to people who hold the titles of the same rank. This process increases the value of ARC, and gives value to the titles themselves, even before considering their future utility in games.
+              </div>
+            </div>
+          </div>
+        </div>
         </div>
         <!-- Project Dependencies -->
         <script src="assets/js/jquery.min.js"></script>
